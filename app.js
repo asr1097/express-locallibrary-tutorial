d5c5712c5172114bcd14fdfb07e7de2c57fbd9eb
Milan <jmilan.97@gmail.com>
2022-02-08 00:37:18 +0100

    deploy files update

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const helmet = require("helmet");
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://asr1097:milan1097@cluster0.3oudg.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://asr1097:milan1097@cluster0.3oudg.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
